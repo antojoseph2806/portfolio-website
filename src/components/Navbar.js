9d5fb11b9fadccde1547ac7abaa1be3219ce3d2c
anto <anto@gmail.com>
2025-05-03 20:24:37 +0530

    changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
+// Import Font Awesome for icons
+import 'font-awesome/css/font-awesome.min.css';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li><Link to="/">Home</Link></li>
+        {/* Home icon instead of text */}
+        <li><Link to="/"><i className="fa fa-home"></i></Link></li> 
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/resume">Resume</Link></li>
